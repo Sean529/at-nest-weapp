a6e1f92f6d46e5fd64e96129fc8bf235c3035cb7
AT <atxiesz@gmail.com>
2022-05-28 16:40:35 +0800

    feat: log4js

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { Log4jsModule } from '@nestx-log4js/core';
 
 @Module({
   imports: [
     UserModule,
+    Log4jsModule.forRoot(),
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/ilovewall'),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Log4jsLogger } from '@nestx-log4js/core';
 import { AppModule } from './app.module';
 
+const logger = new Logger();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  console.log(
-    '%c 🥝 http://localhost:3000 🥝',
-    'font-size:13px; background:#de4307; color:#f6d04d;',
-  );
+  const app = await NestFactory.create(AppModule, {
+    // logger: false,
+  });
+
+  app.useLogger(app.get(Log4jsLogger));
   await app.listen(3000);
 }
-bootstrap();
+
+bootstrap().then(() => {
+  logger.log('🥝 http://localhost:3000 🥝');
+});
