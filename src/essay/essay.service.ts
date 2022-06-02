125d7c056927253819464ac6706d7cdca7da001e
AT <atxiesz@gmail.com>
2022-06-02 22:56:01 +0800

    refactor: 代码优化

--- src/essay/essay.service.ts
+++ src/essay/essay.service.ts
@@ -4,32 +4,33 @@ import { InjectModel } from '@nestjs/mongoose';
 
 import { EssayDocument } from '../schema/essay.schema';
 import { generateId } from '../utils';
+import { IEssay } from './essay.type';
 
 @Injectable()
 export class EssayService {
   constructor(@InjectModel('Essay') private EssayModel: Model<EssayDocument>) {}
 
-  async create(body, user) {
+  // 创建文章
+  create = async (body, user): Promise<IEssay> => {
     const { userId } = user;
     const { content } = body;
-    const essayInfo = await this.generateEssay(userId, content);
-    return essayInfo;
-  }
+    return await this.generateEssay(userId, content);
+  };
 
-  async generateEssay(userId, content) {
-    const essayId = await generateId();
-    const essayInfo = {
+  generateEssay = async (userId: string, content: string): Promise<IEssay> => {
+    const essayId: string = await generateId();
+    const essayInfo: IEssay = {
       essayId,
       userId,
       content,
-      type: 0, // 仅文字
-      auditStatus: 0, // 待审核
+      type: 0,
+      auditStatus: 0,
       isDelete: false,
       deleteTime: null,
       createTime: new Date().getTime(),
       updateTime: new Date().getTime(),
     };
-    const createEssay = new this.EssayModel(essayInfo);
+    const createEssay: EssayDocument = new this.EssayModel(essayInfo);
     return await createEssay.save();
-  }
+  };
 }
---
+++ src/essay/essay.type.ts
@@ -0,0 +1,16 @@
+export enum EssayType {
+  text, // 仅文字
+  image, // 包含文字、图片
+}
+
+export interface IEssay {
+  essayId: string;
+  userId: string;
+  content: string;
+  type: EssayType;
+  auditStatus: number; // 待审核
+  isDelete: boolean;
+  deleteTime: number | null;
+  createTime: number;
+  updateTime: number;
+}
