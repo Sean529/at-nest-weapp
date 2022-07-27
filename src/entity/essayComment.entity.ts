de28da11b02654676f045dd758871f772fe1240b
Sean <atxiesz@gmail.com>
2022-07-27 23:10:31 +0800

    feat: 创建动态评论

--- src/entity/essayComment.entity.ts
+++ src/entity/essayComment.entity.ts
@@ -8,40 +8,33 @@ import {
 
 import { BaseEntity } from './BaseEntity';
 import { UserInfo } from './userInfo.entity';
-import { EssayType } from '../essay/essay.type';
 import { UserEssay } from './userEssay.entity';
-
-export enum AuditStatus {
-  CHECK_PENDING, // 待审核
-  PASS, // 审核通过
-  REJECT, // 审核拒绝
-}
 
 /**
  * 用户信息表
  */
-@Entity({ name: 'essayComment' })
+@Entity({ name: 'essay_comment' })
 export class EssayComment extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
   /**
-   * 文章id
+   * 评论id
    */
   @Column({
     type: 'bigint', // 大数字
     name: 'comment_id',
     unique: true, // 不重复
     unsigned: true, // unsigned
-    comment: '文章id',
+    comment: '评论id',
   })
   commentId: number;
 
   /**
    * 多个评论对应一个文章
    */
-  @ManyToOne(() => UserInfo, (user) => user.userId)
-  @JoinColumn({ name: 'user_id' })
+  @ManyToOne(() => UserEssay, (essay) => essay.essayId)
+  @JoinColumn({ name: 'essay_id' })
   essayInfo: UserEssay;
 
   /**
@@ -60,29 +53,6 @@ export class EssayComment extends BaseEntity {
     length: 300,
   })
   content: string;
-
-  /**
-   * 动态类型, 1:纯文本, 2:图文
-   */
-  @Column({
-    comment: '动态类型, 1:纯文本, 2:图文',
-    type: 'enum',
-    enum: EssayType,
-    default: EssayType.TEXT,
-  })
-  type: number;
-
-  /**
-   * 审核状态,0:待审核,1:通过,2:拒绝
-   */
-  @Column({
-    type: 'enum',
-    enum: AuditStatus,
-    default: AuditStatus.CHECK_PENDING,
-    name: 'audit_status',
-    comment: '审核状态,0:待审核,1:通过,2:拒绝',
-  })
-  auditStatus: number;
 
   /**
    * 是否已删除
